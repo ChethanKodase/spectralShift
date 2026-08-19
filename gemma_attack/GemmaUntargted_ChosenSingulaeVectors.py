8b8247e0ef2c43c7adada1ff30885d8fd292771b
ChethanKodase <chethankr27@gmail.com>
2026-08-19 18:24:05 +0200

    updates

--- gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py
+++ gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py
@@ -88,7 +88,7 @@ cd spectralShift/
 conda activate gemma3
 export PYTHONNOUSERSITE=1
 for ATTACK_SAMPLE in $(seq 33 50); do
-    python gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py --attck_type ImpSamp --desired_norm_l_inf 0.0045 --learningRate 0.001 --num_steps 1000 --attackSample $ATTACK_SAMPLE --AttackStartLayer 0 --numLayerstAtAtime 1 --standardDivCutOff 3
+    python gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py --attck_type ImpSamp --desired_norm_l_inf 0.005 --learningRate 0.001 --num_steps 1000 --attackSample $ATTACK_SAMPLE --AttackStartLayer 0 --numLayerstAtAtime 1 --standardDivCutOff 3
 done
 
 
@@ -98,7 +98,7 @@ cd spectralShift/
 conda activate gemma3
 export PYTHONNOUSERSITE=1
 for ATTACK_SAMPLE in $(seq 33 50); do
-    python gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py --attck_type ImpSamp --desired_norm_l_inf 0.0045 --learningRate 0.001 --num_steps 1000 --attackSample $ATTACK_SAMPLE --AttackStartLayer 0 --numLayerstAtAtime 1 --standardDivCutOff 4
+    python gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py --attck_type ImpSamp --desired_norm_l_inf 0.005 --learningRate 0.001 --num_steps 1000 --attackSample $ATTACK_SAMPLE --AttackStartLayer 0 --numLayerstAtAtime 1 --standardDivCutOff 4
 done
 
 
@@ -108,7 +108,7 @@ cd spectralShift/
 conda activate gemma3
 export PYTHONNOUSERSITE=1
 for ATTACK_SAMPLE in $(seq 33 50); do
-    python gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py --attck_type ImpSamp --desired_norm_l_inf 0.0045 --learningRate 0.001 --num_steps 1000 --attackSample $ATTACK_SAMPLE --AttackStartLayer 0 --numLayerstAtAtime 1 --standardDivCutOff 5
+    python gemma_attack/GemmaUntargted_ChosenSingulaeVectors.py --attck_type ImpSamp --desired_norm_l_inf 0.005 --learningRate 0.001 --num_steps 1000 --attackSample $ATTACK_SAMPLE --AttackStartLayer 0 --numLayerstAtAtime 1 --standardDivCutOff 5
 done
 
 
